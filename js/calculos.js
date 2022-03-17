4e78d6d45100f44b441e22f72bf28f7765bdcbe4
barbara0110 <barbararodrilo@gmail.com>
2022-03-17 10:51:17 +0100

    update

--- js/calculos.js
+++ js/calculos.js
@@ -1,21 +1,47 @@
 // Funcion para leer numeros Reales
 function leerNumero() {
-    num = parseInt(
-        prompt("Porfi, escribe un número")
-    );
+    num = parseInt(prompt("Porfi, escribe un número"));
     return num;
 }
 
-function mostrarResultados() {
-    document.getElementById('td1').innerHTML = n;
-    document.getElementById('td2').innerHTML = factorial();
+function mostrarResultados(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
 }
 
-function factorial() {
+// Ejercicio factorial
+
+function mostrarFactorial(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+
+function factorial(nf) {
     var factorial = 1;
-    for ( i=1; i <= n; i++ ) {
+    for ( i=1; i <= nf; i++ ) {
         factorial = factorial * i;
     }
 
     return factorial;
 }
+
+// Ejercicio múltiplos
+
+function multiplosDeHasta (n, m) {
+    mensaje= "Los múltiplos de " + n + " hasta " + m + " son: ";
+    multiplo=0;
+    for(i=1; multiplo<m; i++) {
+    multiplo= n*i;
+    mensaje = mensaje + multiplo + " ";
+    }
+
+    return mensaje;      
+
+}
+
+function mostrarMultiplos(n, m) {
+    document.getElementById("td11").innerHTML=n;
+    document.getElementById("td12").innerHTML=m;
+    document.getElementById("td13").innerHTML= multiplosDeHasta(n, m);
+}
+
